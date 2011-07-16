92311a8191dfae99de31c92fe79031678e593c15
Kislak Serge <kislak7@gmail.com>
2011-07-16 11:54:11 +0300

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Three::Application.config.secret_token = '09dbbacd4935a58f400683b2dfc75bd1145ad08d06be0cdee97701d279b7be832dd7f7e075e5a9f677f133b05ee7b970ca2eeabbff0297b2fc0ddeaa21d12784'
